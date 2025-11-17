6fe8d698f91a5569cb95838369a8d9452dcccea8
Toadytop <tom.philip.ca@gmail.com>
2025-11-17 01:57:57 -0500

    Implement failure flow of use case.

--- src/main/java/com/studyarc/interface_adapter/login/LoginPresenter.java
+++ src/main/java/com/studyarc/interface_adapter/login/LoginPresenter.java
@@ -4,12 +4,14 @@ import com.studyarc.use_case.login.LoginOutputBoundary;
 import com.studyarc.use_case.login.LoginOutputData;
 
 public class LoginPresenter implements LoginOutputBoundary {
-
+    private LoginViewModel loginViewModel;
 
     @Override
     public void prepareView(LoginOutputData loginOutputData) {
         if(!loginOutputData.isSuccess()){
-
+            loginViewModel.getState().setPassword("");
+            loginViewModel.getState().setErrorCode("Incorrect username or password.");
+            loginViewModel.firePropertyChange();
         }
     }
 }
