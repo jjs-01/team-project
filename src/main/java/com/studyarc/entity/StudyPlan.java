e477d82aa407cd1dafc5e9fcb1fc178093d86963
Nikou131 <achen5502@gmail.com>
2025-11-19 17:45:31 -0500

    minor change to studyplan

--- src/main/java/com/studyarc/entity/StudyPlan.java
+++ src/main/java/com/studyarc/entity/StudyPlan.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class StudyPlan {
     private String title;
     private ArrayList<Milestone> milestones;
-    private Reflection reflection;
 
     public StudyPlan(String title, ArrayList<Milestone> milestones) {
         this.title = title;
