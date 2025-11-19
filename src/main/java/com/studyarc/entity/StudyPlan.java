f5209e0c1a58e165e3bf99002d0e870ba9bb1d73
Nikou131 <achen5502@gmail.com>
2025-11-19 17:26:14 -0500

    implement reflection UI

--- src/main/java/com/studyarc/entity/StudyPlan.java
+++ src/main/java/com/studyarc/entity/StudyPlan.java
@@ -1,4 +1,14 @@
 package com.studyarc.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudyPlan {
+    private final List<Reflection> reflections = new ArrayList<>();
+
+
+
+    public void addReflection(Reflection r) {
+        reflections.add(r);
+    }
 }
