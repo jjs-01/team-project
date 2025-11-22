2113be17dda79a676e843483b7478dd1b3b92ac6
HenryMorton-UofT <henry.morton@mail.utoronto.ca>
2025-11-22 13:43:05 -0500

    Use Case 6 Data Access Interface

--- src/main/java/com/studyarc/use_case/viewing_research_papers/ViewingResearchPapersDataAccessInterface.java
+++ src/main/java/com/studyarc/use_case/viewing_research_papers/ViewingResearchPapersDataAccessInterface.java
@@ -1,8 +1,7 @@
 package com.studyarc.use_case.viewing_research_papers;
+import com.studyarc.entity.ResearchPaper;
+import java.util.List;
 
 public interface ViewingResearchPapersDataAccessInterface {
-
-
-
-
+    List<ResearchPaper> getAllPapers();
 }
