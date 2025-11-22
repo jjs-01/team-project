d5d1f611c640cb6efa24c5cddef9c879147a8834
HenryMorton-UofT <henry.morton@mail.utoronto.ca>
2025-11-22 14:08:28 -0500

    Use Case 6 Output Data

--- src/main/java/com/studyarc/use_case/viewing_research_papers/ViewingResearchPapersOutputData.java
+++ src/main/java/com/studyarc/use_case/viewing_research_papers/ViewingResearchPapersOutputData.java
@@ -1,4 +1,18 @@
 package com.studyarc.use_case.viewing_research_papers;
+import com.studyarc.entity.ResearchPaper;
+import java.util.List;
 
 public class ViewingResearchPapersOutputData {
+
+    private List<ResearchPaper> papers;
+    private boolean hasPapers;
+
+    public void ViewPapersOutputData(List<ResearchPaper> papers, boolean hasPapers) {
+        this.papers = papers;
+        this.hasPapers = hasPapers;
+    }
+
+    public List<ResearchPaper> getPapers() { return papers; }
+    public boolean hasPapers() { return hasPapers; }
+
 }
