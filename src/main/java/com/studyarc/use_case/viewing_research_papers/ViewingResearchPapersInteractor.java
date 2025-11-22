27b49dc109790ba00dda8ce0e4d18de1dc76f85f
HenryMorton-UofT <henry.morton@mail.utoronto.ca>
2025-11-22 16:49:33 -0500

    Use Case 6 simplifications

--- src/main/java/com/studyarc/use_case/viewing_research_papers/ViewingResearchPapersInteractor.java
+++ src/main/java/com/studyarc/use_case/viewing_research_papers/ViewingResearchPapersInteractor.java
@@ -2,24 +2,22 @@ package com.studyarc.use_case.viewing_research_papers;
 import com.studyarc.entity.ResearchPaper;
 import java.util.List;
 
-public class ViewingResearchPapersInteractor implements com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersInputBoundary {
-    private com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersDataAccessInterface repository;
-    private com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersOutputBoundary presenter;
+public class ViewingResearchPapersInteractor implements ViewingResearchPapersInputBoundary {
+    private ViewingResearchPapersDataAccessInterface repository;
+    private ViewingResearchPapersOutputBoundary presenter;
 
-    public void ViewingResearchPapersInteractor(com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersDataAccessInterface repository,
-                                                com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersOutputBoundary presenter) {
+    public void ViewingResearchPapersInteractor(ViewingResearchPapersDataAccessInterface repository,
+                                                ViewingResearchPapersOutputBoundary presenter) {
         this.repository = repository;
         this.presenter = presenter;
     }
 
     @Override
-    public void viewPapers(com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersInputData inputData) {
+    public void viewPapers(ViewingResearchPapersInputData inputData) {
         List<ResearchPaper> papers = repository.getAllPapers();
         boolean hasPapers = !papers.isEmpty();
-        com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersOutputData outputData =
-                new com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersOutputData(papers, hasPapers);
-
-
+        ViewingResearchPapersOutputData outputData =
+                new ViewingResearchPapersOutputData(papers, hasPapers);
         presenter.present(outputData);
 
 
