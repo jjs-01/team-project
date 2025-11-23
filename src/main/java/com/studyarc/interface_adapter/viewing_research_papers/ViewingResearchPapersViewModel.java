32df46babba37561f76daa062a9e1e21288279f7
HenryMorton-UofT <henry.morton@mail.utoronto.ca>
2025-11-22 22:04:56 -0500

    Use Case 6 View

--- src/main/java/com/studyarc/interface_adapter/viewing_research_papers/ViewingResearchPapersViewModel.java
+++ src/main/java/com/studyarc/interface_adapter/viewing_research_papers/ViewingResearchPapersViewModel.java
@@ -1,4 +1,35 @@
 package com.studyarc.interface_adapter.viewing_research_papers;
+import com.studyarc.entity.ResearchPaper;
+import com.studyarc.use_case.viewing_research_papers.ViewingResearchPapersInteractor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewingResearchPapersViewModel {
+    private ViewingResearchPapersInteractor interactor;
+    private List<ResearchPaper> researchPapers;
+
+    public void ViewResearchPapersViewModel(ViewingResearchPapersInteractor interactor) {
+        this.interactor = interactor;
+        this.researchPapers = new ArrayList<>();
+    }
+
+
+    public void loadResearchPapers() {
+        List<ResearchPaper> papersFromInteractor = interactor.fetchResearchPapers();
+        if (papersFromInteractor != null) {
+            this.researchPapers = papersFromInteractor;
+        } else {
+            this.researchPapers = new ArrayList<>();
+        }
+    }
+
+    /**
+     * Returns the current list of research papers.
+     * The view calls this when updating the table.
+     */
+    public List<ResearchPaper> getResearchPapers() {
+        return researchPapers;
+    }
+}
 }
--- src/main/java/com/studyarc/view/ViewingResearchPapersView.java
+++ src/main/java/com/studyarc/view/ViewingResearchPapersView.java
@@ -1,28 +1,47 @@
 package com.studyarc.view;
 
-import javax.swing.*;
-import java.awt.*;
+import com.studyarc.entity.ResearchPaper;
+import com.studyarc.interface_adapter.viewing_research_papers.ViewingResearchPapersViewModel;
 
-public class ViewingResearchPapersView extends JPanel{
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
+
+public class ViewingResearchPapersView extends JPanel {
     private JLabel titleLabel;
     private JTable papersTable;
+    private DefaultTableModel tableModel;
+    private ViewingResearchPapersViewModel viewModel;
 
-    public ViewingResearchPapersView() {
+    public ViewingResearchPapersView(ViewingResearchPapersViewModel viewModel) {
+        this.viewModel = viewModel;
         this.setLayout(new BorderLayout());
 
-        // Title Section at the top
         JPanel topPanel = new JPanel();
         titleLabel = new JLabel("Research Papers");
         titleLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
         topPanel.add(titleLabel);
-
         this.add(topPanel, BorderLayout.NORTH);
 
-        // Research Paper list section
-        String[] columns = {"Data1", "Data2", "......", "Data n"};
-        Object[][] data = {};
-        papersTable = new JTable(data, columns);
+        String[] columns = {"Title", "Authors", "URL"};
+        tableModel = new DefaultTableModel(columns, 0);
+        papersTable = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(papersTable);
         this.add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public void refreshTable() {
+        tableModel.setRowCount(0);
+
+        List<ResearchPaper> papers = viewModel.getResearchPapers();
+        for (ResearchPaper paper : papers) {
+            Object[] row = {
+                    paper.getTitle(),
+                    paper.getAuthors(),
+                    paper.getUrl()
+            };
+            tableModel.addRow(row);
+        }
     }
 }
