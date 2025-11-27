e1c06b9a76b6c936e7527b842246bd0c042cd267
Toadytop <tom.philip.ca@gmail.com>
2025-11-27 00:56:03 -0500

    Add focus to the user class and constructors, and properly do hash validation.

--- src/main/java/com/studyarc/entity/User.java
+++ src/main/java/com/studyarc/entity/User.java
@@ -3,15 +3,48 @@ package com.studyarc.entity;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Random;
 
 public class User {
     private byte[] salt;
     private byte[] passwordHash;
+    private String username;
+
+    private String focus;
+
+    public User(String username, String password, String focus) throws NoSuchAlgorithmException {
+        Random rand = new SecureRandom();
+        byte[] salt = new byte[16];
+        rand.nextBytes(salt);
+        this.username = username;
+        this.salt = salt;
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes(StandardCharsets.UTF_8));
+        md.update(this.salt);
+        this.passwordHash = md.digest();
+        this.focus = focus;
+    }
+
+    public User(String username, String password, byte[] salt, String focus) throws NoSuchAlgorithmException {
+        this.username = username;
+        this.salt = salt;
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes(StandardCharsets.UTF_8));
+        md.update(this.salt);
+        this.passwordHash = md.digest();
+        this.focus = focus;
+    }
 
     public boolean validateHash(String password) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(password.getBytes(StandardCharsets.UTF_8));
         md.update(this.salt);
-        return md.digest()==this.passwordHash;
+        return Arrays.equals(md.digest(), this.passwordHash);
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
