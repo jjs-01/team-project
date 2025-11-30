4cf4360c3a002c9d5b1506b877fc006a7b554c00
Toadytop <tom.philip.ca@gmail.com>
2025-11-30 15:52:06 -0500

    Place registration logic in the DAO.

--- src/main/java/com/studyarc/use_case/login/LoginDataAccessInterface.java
+++ src/main/java/com/studyarc/use_case/login/LoginDataAccessInterface.java
@@ -2,13 +2,11 @@ package com.studyarc.use_case.login;
 
 import com.studyarc.entity.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface LoginDataAccessInterface {
-    boolean registerUser(User u);
+    boolean registerUser(String username, String password);
     User getUser(String username);
     void setUser(User u);
     void save();
-    List<User> getAllUsers();
 }
