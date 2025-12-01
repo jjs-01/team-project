79e205d2efc13a6565082e1b3f773c14ea06b8a9
jules <julia.sinclair.workemail@gmail.com>
2025-12-01 16:31:48 -0500

    LoadMilestone successTest

--- src/test/java/use_case/load_milestones/LoadMilestoneInteractorTest.java
+++ src/test/java/use_case/load_milestones/LoadMilestoneInteractorTest.java
@@ -3,7 +3,6 @@ package use_case.load_milestones;
 import com.studyarc.data_access.InMemoryDataUserDataAccessObject;
 import com.studyarc.entity.Milestone;
 import com.studyarc.entity.StudyPlan;
-import com.studyarc.interface_adapter.load_milestones.LoadMilestonesPresenter;
 import com.studyarc.use_case.load_milestones.*;
 
 import org.junit.Test;
@@ -22,7 +21,7 @@ public class LoadMilestoneInteractorTest {
 
         // Add test milestones to the studyPlan
         studyPlan.getMilestones().add(new Milestone("milestone 1", "XX/XX/XXXX"));
-        studyPlan.getMilestones().add(new Milestone("milestone 2", "XX/XX/XXXX"));
+        studyPlan.getMilestones().add(new Milestone("milestone 2", "XX/XX/2025"));
 
         userRepository.getPlans().add(studyPlan);
 
@@ -31,7 +30,12 @@ public class LoadMilestoneInteractorTest {
         LoadMilestonesOutputBoundary successPresenter = new LoadMilestonesOutputBoundary() {
             @Override
             public void prepareSuccessView(LoadMilestonesOutputData outputData) {
-                assertEquals(outputData.getMilestoneInfo().get(0), 3);
+                List<String[]> milestoneInfo = outputData.getMilestoneInfo();
+                assertEquals(2, milestoneInfo.size());
+                assertEquals("milestone 1", milestoneInfo.get(0)[0]);
+                assertEquals("milestone 2", milestoneInfo.get(1)[0]);
+                assertEquals("XX/XX/XXXX", milestoneInfo.get(0)[1]);
+                assertEquals("XX/XX/2025", milestoneInfo.get(1)[1]);
             }
 
             @Override
